63c61e1f668d2d231de933f5396e770a99b330b7
Aaron Boyd <aaronmboyd@gmail.com>
2019-12-07 14:22:34 +0100

    Update create_protonvpn_service.sh

--- create_protonvpn_service.sh
+++ create_protonvpn_service.sh
@@ -1,16 +1,14 @@
 sudo tee /etc/systemd/system/protonvpn.service <<EOF
 [Unit]
 Description=ProtonVPN CLI Auto-Start
-After=network.target
+Wants=network-online.target
 
 [Service]
 Type=forking
-User=root
+Environment=SUDO_USER=<user where protonvpn is initialised>
 ExecStart=/usr/local/bin/protonvpn c IS-DE#1 || /usr/local/bin/protonvpn c IS-NL#1 || /usr/local/bin/protonvpn c IS-IL#1
-ExecReload=/usr/local/bin/protonvpn disconnect && /usr/local/bin/protonvpn c IS-DE#1 || /usr/local/bin/protonvpn c IS-NL#1 || /usr/local/bin/protonvpn c IS-IL#1
+ExecReload=/usr/local/bin/protonvpn c IS-DE#1 || /usr/local/bin/protonvpn c IS-NL#1 || /usr/local/bin/protonvpn c IS-IL#1
 ExecStop=/usr/local/bin/protonvpn disconnect
-Restart=always
-KillMode=mixed
 
 [Install]
 WantedBy=multi-user.target
