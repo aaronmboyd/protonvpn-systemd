887ff003a4b91476e9dbb86532a6d501eb026a05
Aaron Boyd <aaronmboyd@gmail.com>
2019-12-07 14:33:30 +0100

    Update create_protonvpn_service.sh

--- create_protonvpn_service.sh
+++ create_protonvpn_service.sh
@@ -1,3 +1,6 @@
+# Create file protonvpn.service
+# Make sure you replace the Environment variable with the 
+# username where you installed and first called "protonvpn init"
 sudo tee /etc/systemd/system/protonvpn.service <<EOF
 [Unit]
 Description=ProtonVPN CLI Auto-Start
